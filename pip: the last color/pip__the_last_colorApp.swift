c375513937b43a2898c2c954f8dcb8614dee16d2
fahmi <fahmianton@gmail.com>
2026-09-11 15:51:36 +0700

    Initial Commit

---
+++ pip: the last color/pip__the_last_colorApp.swift
@@ -0,0 +1,17 @@
+//
+//  pip__the_last_colorApp.swift
+//  pip: the last color
+//
+//  Created by Muhammad Fahmi on 11/09/26.
+//
+
+import SwiftUI
+
+@main
+struct pip__the_last_colorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
